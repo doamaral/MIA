06518222ca510f4b1864811a4800e5028a2875ae
Lucas Amaral <lopesdoamaral@gmail.com>
2015-02-27 14:09:23 -0300

    Alteração do metodo de atribuição de Variaveis

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import sort_methods
 
 vetor = [10, 1, 3, 2, 15, 17, 13]
 
-a = sort_methods.bubble(vetor)
+#a = sort_methods.bubble(vetor)
 b = sort_methods.insertion(vetor)
 
-print(a)
-
+#print(a)
 print(b)
--- sort_methods.py
+++ sort_methods.py
@@ -4,9 +4,7 @@ def bubble(v):
     for i in range(len(v)):
         for j in range(i+1, len(v)):
             if(v[i] > v[j]):
-                aux = v[i]
-                v[i] = v[j]
-                v[j] = aux
+                v[i], v[j] = v[j], v[i]
     return v
 
 def insertion(v):
@@ -14,8 +12,6 @@ def insertion(v):
         j = i - 1
         chave = v[i]
         while v[j] > chave and j >= 0:
-            aux = v[j]
-            v[j] = v[j+1]
-            v[j+1] = aux
+            v[j], v[j+1] = v[j+1], v[j]
             j = j - 1
     return v
